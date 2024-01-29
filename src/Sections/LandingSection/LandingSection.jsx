06bfbb6a30cb6e06ccfc8680babccb5a48d11535
adlane <adlaneould@gmail.com>
2024-01-29 19:04:50 +0100

    issue with astronaut on loading

--- src/Sections/LandingSection/LandingSection.jsx
+++ src/Sections/LandingSection/LandingSection.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './LandingSection.css'
 import MoonField from '../../media/Fields/MoonField.png'
+import ManKneesFlag from '../../media/Moon/Man/man-knees-flag.png'
 
 class LandingSection extends Component {
     render() {
         return (
             <div className='landing-section-master'>
                 <img src={MoonField} alt="" />
+                <img src={ManKneesFlag} alt=""  className='man-flag'/>
             </div>
         );
     }
