b4edf2000ed05fcad74c6651e4b84e7799f67f00
adlane <adlaneould@gmail.com>
2024-01-30 13:38:43 +0100

    remove Moon from UI

--- src/Sections/LandingSection/LandingSection.jsx
+++ src/Sections/LandingSection/LandingSection.jsx
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 import './LandingSection.css'
 import MoonField from '../../media/Fields/MoonField.png'
 import ManKneesFlag from '../../media/Moon/Man/man-knees-flag.png'
-
+import Rocket from '../../media/Moon/Rocket/rocket.png'
+// NOTE - May not be the best Idea
 class LandingSection extends Component {
     render() {
         return (
             <div className='landing-section-master'>
                 <img src={MoonField} alt="" />
                 <img src={ManKneesFlag} alt=""  className='man-flag'/>
+                <img src={Rocket} alt="" className='rocket'/>
             </div>
         );
     }
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -4,7 +4,6 @@ import BandeauSection from '../../sections/Bandeau/BandeauSection';
 import LoadingAnimation from '../../components/LoadingAnimation/LoadingAnimation';
 import TextAnimation from '../../components/TextAnimation/TextAnimation';
 import HomeSection from '../../sections/HomeSection/HomeSection';
-import LandingSection from '../../sections/LandingSection/LandingSection';
 
 class HomePage extends Component {
     render() {
@@ -15,7 +14,6 @@ class HomePage extends Component {
                     <div className="appear-after-loading">
                     <HomeSection />
                     <BandeauSection />  
-                    <LandingSection />
                     </div>
             </div>
         );
